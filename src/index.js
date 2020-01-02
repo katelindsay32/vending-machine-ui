71d12f3f2165b2b6fc8d0926724bb005a41b9c9d
Kate Sickora <katelindsay32@gmail.com>
2020-01-01 21:03:30 -0600

    moving script tags to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
+window.onload = function () {
+    const btn = document.getElementById('dollar');
+    console.log("found obj " + btn);
+    function logMessage() {
+        console.log("hi");
+        InsertDollar();
+    };
 
-function InsertDollar() {
-    console.log("Quarter inserted!");
-}
-export default InsertDollar;
+    function InsertDollar() {
+        console.log("Quarter inserted!");
+    }
+
+    btn.onclick = logMessage;
+};
+
+
+
